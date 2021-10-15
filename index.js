39e18bbbe591b5018550dd773d2c1ee314e98768
JuanjoORD <39684335+JuanjoORD@users.noreply.github.com>
2021-10-15 13:22:57 -0600

    comando start agregado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mysql      = require('mysql');
-const PORT = process.evn.PORT || 3050;
+const PORT = process.env.PORT || 3000;
 const app = express()
 
 app.use(express.json());
